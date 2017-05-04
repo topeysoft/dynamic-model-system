fa596d8af8373dfbc00231adbc57c145fa372c14
topeysoft <temi.adeyeri@gmail.com>
2017-05-04 11:37:34 -0500

    Update authorize.ts

--- src/middlewares/security/authorize.ts
+++ src/middlewares/security/authorize.ts
@@ -1,8 +1,6 @@
 import { Express, Request } from 'express';
 
 const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-const jwtAuthz = require('express-jwt-authz');
 
 export class Authorize {
     /**
@@ -13,20 +11,8 @@ export class Authorize {
     }
 
     private setup(app: Express) {
-        // const checkJwt = jwt({
-        //     secret: jwksRsa.expressJwtSecret({
-        //         cache: true,
-        //         rateLimit: true,
-        //         jwksRequestsPerMinute: 5,
-        //         jwksUri: `https://topeysoft.auth0.com/.well-known/jwks.json`
-        //     }),
-
-        //     audience: 'http://api.topeysoft.local',
-        //     issuer: "https://topeysoft.auth0.com/",
-        //     algorithms: ['HS256']
-        // });
         app.use(jwt({
-            secret: "B8Wbhz81Gar4dr9xzhTvXUJOzHmTCteT8dkl98YYCjDX46SCd7a3tb-UO-nUXuTE",
+            secret: "super-secret here",
            credentialsRequired: false,
             getToken: function fromHeaderOrQuerystring(req:Request) {
                 if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
@@ -36,11 +22,10 @@ export class Authorize {
                 }
                 return null;
             },
-            audience: 'wTqZC92ef4HThbA8muZsA8LI66ddYji3',
-            issuer: "https://topeysoft.auth0.com/",
+            audience: 'your audience id here',
+            issuer: "https://issuer.example.com/",
             algorithms: ['HS256']
         }));
-        // app.use(checkJwt);
     }
 
 }
